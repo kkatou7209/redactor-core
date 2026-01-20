dc16fc561e1ca0fa6ba5303b6d4213d3b45f1528
kkatou7209 <kkatou7209@gmail.com>
2026-01-20 23:33:13 +0900

    add test case to character code

--- src/value/literal_char/escape_sequence/character_code.rs
+++ src/value/literal_char/escape_sequence/character_code.rs
@@ -24,9 +24,13 @@ impl CharacterCode {
     /// Converts the character code to a u32 value.
     pub fn as_u32(&self) -> u32 {
 
+        if self.0.len() < 2 {
+            panic!("Character code is too short: {:?}", self.0);
+        }
+
         let mut value: u32 = 0;
         
-        for &byte in &self.0 {
+        for &byte in &self.0[1..] {
 
             if !byte.is_ascii_digit() {
                 panic!("Character code contains non-digit byte: {:?}", self.0);
@@ -36,5 +40,17 @@ impl CharacterCode {
         }
 
         value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CharacterCode;
+
+    #[test]
+    fn should_create_valid_character_code() {
+        let char_code = CharacterCode::new(b"\\123".to_vec());
+        assert_eq!(char_code.as_bytes(), b"\\123");
+        assert_eq!(char_code.as_u32(), 123);
     }
 }
